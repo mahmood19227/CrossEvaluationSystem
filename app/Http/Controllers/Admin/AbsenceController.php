6283cd06146f5fe9876c73fc5987921fadcccbd7
Mahmood <farokhian@gmail.com>
2019-07-06 15:02:20 +0430

    change order of presentation

--- app/Http/Controllers/Admin/AbsenceController.php
+++ app/Http/Controllers/Admin/AbsenceController.php
@@ -25,7 +25,7 @@ class AbsenceController extends \App\Http\Controllers\Controller
     public function absenceRegForm()
     {
         $users = User::where('usertype','!=','1')->orderby('name')->get();
-        $presentation = Presentation::join('users','userid','=','users.id')->orderby('users.name')->get();
+        $presentation = Presentation::join('users','userid','=','users.id')->orderby('presentation_date')->get();
         return view('admin.register_absence',['users'=>$users,'presentations'=>$presentation]);
     }
 
